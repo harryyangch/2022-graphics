709ca456160ecb207b9df417c637cc47d7a4f962
harryyangch <harryzz12345@gmail.com>
2022-03-21 16:21:52 +0800

    week05

---
+++ week05/main.cpp
@@ -0,0 +1,43 @@
+#include <GL/glut.h>
+float angle=0;
+void display()
+
+{
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();///備份矩陣
+        glRotatef(angle,0,0,1);///轉180度
+        glColor3f(1,1,0);
+        glutSolidTeapot(0.3);
+    glPushMatrix();///備份矩陣
+    glutSwapBuffers();
+
+}
+void motion(int x,int y)
+{
+    angle=x;
+    display();
+}
+
+int main(int argc,char**argv)
+
+{
+
+    glutInit(&argc,argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+
+    glutCreateWindow("week05");
+
+
+
+    glutDisplayFunc(display);
+
+    glutMotionFunc(motion);
+
+    glutMainLoop();
+
+    return 0;
+
+}
+
---
+++ week05_mouse_motion_rotate/main.cpp
@@ -0,0 +1,49 @@
+#include <GL/glut.h>
+float angle=0,oldX=0;
+void display()
+
+{
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();///備份矩陣
+        glRotatef(angle,0,0,1);///轉180度
+        glColor3f(1,1,0);
+        glutSolidTeapot(0.3);
+    glPushMatrix();///備份矩陣
+    glutSwapBuffers();
+
+}
+void mouse(int button,int state,int x,int y)
+{///按下mouse 放開mouse
+    oldX=x;
+}
+
+void motion(int x,int y)
+{ ///拖著它動
+    angle+=(x-oldX);
+    oldX=x;
+    display();
+}
+
+int main(int argc,char**argv)
+
+{
+
+    glutInit(&argc,argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+
+    glutCreateWindow("week05");
+
+
+
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+
+    glutMainLoop();
+
+    return 0;
+
+}
+
